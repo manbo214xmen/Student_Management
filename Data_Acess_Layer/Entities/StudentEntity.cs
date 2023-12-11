2613e1b1a2746256933dc5ae03a8fcbfb18adfb1
ArthurSeventh <hao.vv2001@gmail.com>
2023-12-11 16:19:34 +0700

    Basic_Relationship

--- Data_Acess_Layer/Entities/StudentEntity.cs
+++ Data_Acess_Layer/Entities/StudentEntity.cs
@@ -1,11 +1,26 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace Data_Acess_Layer
 {
 	public class StudentEntity
     {
-		public StudentEntity()
-		{
-		}
-	}
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int StudentId { get; set; }
+        public string StudentName { get; set; }
+        public string StudentEmail { get; set; }
+        public string StudentPhone { get; set; }
+        public int StudentAge { get; set; }
+        public int CurrentGradeId { get; set; }
+        public GradeEntity CurrentGrade { get; set; }
+        public virtual ICollection<CourseEntity> Courses { get; set; }
+
+        public StudentEntity()
+        {
+            this.Courses = new HashSet<CourseEntity>();
+        }
+    }
 }
 
